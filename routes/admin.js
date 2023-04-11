feec96781222b51edf871582df0edd8bedfbc0be
Priya Prasad <ppiya1994@gmail.com>
2023-04-11 09:04:54 +0530

    test

--- routes/admin.js
+++ routes/admin.js
@@ -11,6 +11,7 @@ var storage = multer.diskStorage({
     filename: function (req, file, cb) {
       cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
     }
+    
 })
 
 var upload = multer({ storage: storage }).single('myImage');
